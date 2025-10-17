26adb35d0642d940b8b2ddd3af77929cc216e371
Luis-Bryan1905 <luis190503@gmail.com>
2025-10-17 17:34:51 +0100

    First Three.js Scene

---
+++ Resources/JS/script.js
@@ -0,0 +1,114 @@
+import * as THREE from "three"; // Import Three.js
+
+const scene = new THREE.Scene(); // Initialise 3D scene
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // Create Camera object
+
+const renderer = new THREE.WebGLRenderer(); // Initialise 3D renderer
+
+renderer.setSize( window.innerWidth, window.innerHeight ); // Set Render Size
+
+renderer.setAnimationLoop( animate ); // Start animation
+
+document.body.appendChild( renderer.domElement ); // add the renderer to the HTML
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 ); // Create new geometry met and set its X, Y & Z scale
+
+const sphereGeometry = new THREE.SphereGeometry( 0.65, 16, 16 );  // Create new geometry met and set its X, Y & Z scale
+
+const torusGeometry = new THREE.TorusGeometry( 1.5, 0.3, 16, 116 ); 
+
+const material = new THREE.MeshBasicMaterial // Basic Material
+( { 
+    color: 0x00ff00 // Base Colour
+} );
+
+const material2 = new THREE.MeshBasicMaterial // Basic Material
+( { 
+    color: 0xFF0000 // Base Colour
+} );
+
+const material3 = new THREE.MeshBasicMaterial // Basic Material
+( { 
+    color: 0xCC6CE7 // Base Colour
+} );
+
+const material4 = new THREE.MeshLambertMaterial // Non-physically based Lambertian Material
+( { 
+    color: 0x54c0da, // Base Colour
+    emissive: 0x09232a, // Emissive colour
+    reflectivity: 1, // How much the environment map affects the surfac
+    refractionRatio: 1, // Index of refraction (IOR)
+    
+} );
+
+const material5 = new THREE.MeshPhongMaterial // Material that can simulate shiny surfaces with specular highlights
+( { 
+    color: 0xfcc200  , // Base Colour
+    specular: 0xffffff, //Specular colour
+    shininess: 100, // How shiny the material is
+    emissive: 0x422c00, // Emissive colour
+    reflectivity: 1, // How much the environment map affects the surface
+    refractionRatio: 1, // Index of refraction (IOR)
+    
+} );
+
+const cube = new THREE.Mesh( geometry, material ); // Create cube object and set material
+const cube2 = new THREE.Mesh( geometry, material3 ); // Create cube object and set material
+const torus  = new THREE.Mesh( torusGeometry, material5 ); // Create torus object and set material
+const sphere  = new THREE.Mesh( sphereGeometry, material2 ); // Create sphere object and set material
+
+
+const Light = new THREE.DirectionalLight( 0xffffff, 3 ); // soft white light// White directional light at half intensity shining from the top.
+
+scene.add( Light ); // Add directional light to scene
+scene.add( cube ); // Add cube to scene
+scene.add( cube2 ); // Add cube to scene
+scene.add( sphere ); // Add sphere to scene
+scene.add( torus ); // Add torus  to scene
+
+let MovingLeft = false; // which way torus is moving
+let MoveSpeed = 0.025; // movement speed
+let CubeAnimationSpeed = 0.025; // Cube animation speed
+let TorusAnimationSpeed = 0.02; // Torus animation speed
+
+camera.position.z = 3; // Camera Distance
+
+function animate() { // Animation Function
+
+  cube.rotation.x += CubeAnimationSpeed; // rotate cube
+
+  cube2.rotation.y += CubeAnimationSpeed; // rotate cube
+
+  torus.rotation.x += TorusAnimationSpeed; // rotate torus
+
+  torus.rotation.y += TorusAnimationSpeed; // rotate torus
+
+    if (MovingLeft == false) 
+    {
+        torus.position.x += MoveSpeed; // Move torus right
+
+        if (torus.position.x >= 3) // right boundary
+            { 
+
+            MovingLeft = true;
+            
+            }
+    } 
+
+    else 
+
+    {
+        torus.position.x -= MoveSpeed; // Move torus left
+
+        if (torus.position.x <= -3) // left boundary
+            { 
+
+            MovingLeft = false;
+
+            }
+    }
+
+  renderer.render( scene, camera ); // Render Scene
+
+}
